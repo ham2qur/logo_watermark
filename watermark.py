43d9f8fc9623afad0374b2736cfe1504601eb6a9
Hammad Hussain <ham2qur@hotmail.com>
2022-08-02 00:17:52 +0500

    Added method 2 for watermarking photo

--- watermark.py
+++ watermark.py
@@ -36,25 +36,33 @@ def watermark():
 
 
 def watermark2():
-    base_image = Image.open(io.BytesIO(base64.b64decode(image_b64))).convert("RGBA")
+    """
+    Method 2: Method to watermark photos using the Image.alpha_composite
+    """
+
+    base_image = Image.open("photo.jpeg").convert("RGBA")
     transparent = Image.new('RGBA', base_image.size, (0, 0, 0, 0))
 
     (width, height) = base_image.size
-    watermark = Image.open(io.BytesIO(base64.b64decode(logo_b64)))
-    watermark = watermark.resize((int(width / 2), int(height / 2)))
-    watermark = self.add_image_transparency(watermark)
-    position = get_center_position(transparent, watermark)
-    transparent.paste(watermark, position, mask=watermark)
+    logo_image = Image.open("logo.png")
+    logo_image = logo_image.resize((int(width / 2), int(height / 2)))
+    logo_image = add_image_transparency(logo_image)
+    position = get_center_position(transparent, logo_image)
+    transparent.paste(logo_image, position, mask=logo_image)
 
+    # Instead of pasting one image over another we can also use alpha_composite to put images on layers
     watermarked = Image.alpha_composite(base_image, transparent)
     im_file = io.BytesIO()
+
+    # RGBA mode don't support JPEG so need to use PNG here.
     watermarked.save(im_file, format="PNG")
-    im_bytes = im_file.getvalue()  # im_bytes: image in binary format.
+    watermarked.show()
+    im_bytes = im_file.getvalue()
     return base64.b64encode(im_bytes)
 
 
 def add_image_transparency(image, alpha=128):
-    # Load image and extract alpha channel
+    # To add transparency we need to make non zero alpha pixels to given alpha value.
     image_alpha = image.getchannel('A')
     new_alpha = image_alpha.point(lambda i: alpha if i > 0 else 0)
     image.putalpha(new_alpha)
@@ -62,6 +70,7 @@ def add_image_transparency(image, alpha=128):
 
 
 def get_center_position(image, image2):
+    # Get the center positions of image for image2
     width, height = image.size
     width2, height2 = image2.size
     x = (width - width2) / 2
@@ -71,3 +80,4 @@ def get_center_position(image, image2):
 
 if __name__ == "__main__":
     watermark()
+    watermark2()
